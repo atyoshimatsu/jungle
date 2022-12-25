62b6a6ddb36c12c540058ee2d382a62d4b21e475
atyoshimatsu <at.yoshimatsu@gmail.com>
2022-12-24 20:25:29 -0800

    Modify signup form to user first and last name

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,4 @@
 class UsersController < ApplicationController
-  has_secure_password
-
   def new
   end
 
@@ -17,7 +15,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
 
 end
