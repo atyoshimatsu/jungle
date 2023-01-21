5b63b548a226b462f966a389c1cf213c7bf7bb46
atyoshimatsu <at.yoshimatsu@gmail.com>
2023-01-20 17:17:36 -0800

    Add unique constraint to migration file and do migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_25_032708) do
+ActiveRecord::Schema.define(version: 2023_01_21_004257) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 2022_12_25_032708) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "line_items", "orders"
