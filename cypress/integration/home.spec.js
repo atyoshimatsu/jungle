bb022ee28b03aba0909c88f74e044e60fd8e69bf
atyoshimatsu <at.yoshimatsu@gmail.com>
2023-01-21 14:39:49 -0800

    Create home cypress test

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,8 @@
+describe('jungle app', () => {
+  beforeEach(() => {
+    cy.visit('http://127.0.0.1:3000')
+  })
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+})
