3db081586be67d653f68a9ed3cc5f58acafc38bd
atyoshimatsu <at.yoshimatsu@gmail.com>
2023-01-20 19:44:05 -0800

    Add a class method to the user model for authentication users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   validates :email, uniqueness: { case_sensitive: false }
   validates :email, :first_name, :last_name, presence: true
   validates :password, :password_confirmation, length: { minimum: 8 }
+
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by_email(email.strip.downcase)
+    # If the user exists AND the password entered is correct.
+    if user && user.authenticate(password)
+      user
+    end
+  end
 end
