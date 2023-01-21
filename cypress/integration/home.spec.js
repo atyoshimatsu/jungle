ca3e2ad8b69e183ce50dd0709b7648376f5b31e5
atyoshimatsu <at.yoshimatsu@gmail.com>
2023-01-21 15:52:09 -0800

    Change the home url in the home spec test

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,6 +1,6 @@
 describe('jungle app', () => {
   beforeEach(() => {
-    cy.visit('http://127.0.0.1:3000')
+    cy.visit('http://0.0.0.0:3000')
   })
   it("There is products on the page", () => {
     cy.get(".products article").should("be.visible");
