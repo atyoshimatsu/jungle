4b3786f79246464aff487836180aa7bfea1659ab
atyoshimatsu <at.yoshimatsu@gmail.com>
2023-01-20 17:45:48 -0800

    Add length constraint to password and password confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   has_secure_password
   validates :email, uniqueness: { case_sensitive: false }
   validates :email, :first_name, :last_name, presence: true
+  validates :password, :password_confirmation, length: { minimum: 8 }
 end
