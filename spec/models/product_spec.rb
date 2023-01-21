199443415bac37e2b7385050e3808ced8bae00ae
atyoshimatsu <at.yoshimatsu@gmail.com>
2023-01-20 16:00:39 -0800

    Create the product_spec test

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  describe 'Validations' do
+    it 'is valid with four fields' do
+      @category = Category.new(name: 'test')
+      @product = Product.new(name: 'sample', price_cents: 100, quantity: 1, category: @category)
+      expect(@product).to be_valid
+    end
+
+    it 'should be invalid without a name' do
+      @category = Category.new(name: 'test')
+      @product = Product.new(name: nil, price_cents: 100, quantity: 1, category: @category)
+      expect(@product).to be_invalid
+      expect(@product.errors.full_messages).to include("Name can't be blank")
+    end
+
+    it 'should be invalid without a price' do
+      @category = Category.new(name: 'test')
+      @product = Product.new(name: 'sample', price_cents: nil, quantity: 1, category: @category)
+      expect(@product).to be_invalid
+      expect(@product.errors.full_messages).to include("Price cents can't be blank")
+    end
+
+    it 'should be invalid without a quantity' do
+      @category = Category.new(name: 'test')
+      @product = Product.new(name: 'sample', price_cents: 100, quantity: nil, category: @category)
+      expect(@product).to be_invalid
+      expect(@product.errors.full_messages).to include("Quantity can't be blank")
+    end
+
+    it 'should be invalid without a price_cents' do
+      @category = Category.new(name: 'test')
+      @product = Product.new(name: 'sample', price_cents: 100, quantity: 1, category: nil)
+      expect(@product).to be_invalid
+      expect(@product.errors.full_messages).to include("Category can't be blank")
+    end
+
+  end
+end
