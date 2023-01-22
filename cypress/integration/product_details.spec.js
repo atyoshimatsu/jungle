c76c04464806d017ed6e375fbd2f4bad5013e13c
atyoshimatsu <at.yoshimatsu@gmail.com>
2023-01-21 16:39:08 -0800

    Add semicolons

--- cypress/integration/product_details.spec.js
+++ cypress/integration/product_details.spec.js
@@ -1,9 +1,9 @@
 describe('jungle app', () => {
   beforeEach(() => {
-    cy.visit('http://0.0.0.0:3000')
+    cy.visit('http://0.0.0.0:3000');
   })
   it("should go to the product page when the link is clicked", () => {
-    cy.get('.products').children('article').first().click()
+    cy.get('.products').children('article').first().click();
     cy.url().should('match', /http:\/\/0.0.0.0:3000\/products\/\d+/);
   });
 })
